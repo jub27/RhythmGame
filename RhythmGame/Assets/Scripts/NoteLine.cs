142005bac73a70424516cbf1b7259812a2c32c12
JaeyeopKim <jykim@neofect.com>
2023-03-09 19:25:24 +0900

    입력시 노트라인 알파값 변화 기능 구현

--- RhythmGame/Assets/Scripts/NoteLine.cs
+++ RhythmGame/Assets/Scripts/NoteLine.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NoteLine : MonoBehaviour
 {
     [SerializeField]
     private RectTransform noteStartTransform;
+    private Image image;
     private Vector3 noteStartPosition;
+
+    private Color originColor;
+    private Color activeColor;
 
     private void Awake()
     {
         noteStartPosition = noteStartTransform.position;
+        image = GetComponent<Image>();
+        originColor = image.color;
+        activeColor = originColor;
+        activeColor.a = 0.7f;
+    }
+
+    public void OnKeyInput()
+    {
+        image.color = activeColor;
     }
 
     public void SetNote(Note note)
